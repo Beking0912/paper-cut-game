806cda89ee955b60777d03a4ae571c8875516e85
Beking0912 <sbqoo@qq.com>
2024-01-12 12:01:45 -0500

    Initial Commit

---
+++ PaperCutGame/PaperCutGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperCutGameApp.swift
+//  PaperCutGame
+//
+//  Created by Beking on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PaperCutGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
