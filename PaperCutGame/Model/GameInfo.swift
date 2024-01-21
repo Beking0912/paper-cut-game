345451b01bbf02e9c142b464743960f702ed5a92
Beking0912 <sbqoo@qq.com>
2024-01-21 15:03:12 -0500

    add one more round

--- PaperCutGame/Model/GameInfo.swift
+++ PaperCutGame/Model/GameInfo.swift
@@ -19,10 +19,11 @@ struct GameInfo: Identifiable {
 }
 
 var GameInfos : [GameInfo] = [
-    GameInfo(round: 1, difficulty: "★☆☆", score: 20, paperImage: "round1/foldpaper", mainImage: "round1/main", optionImages: ["round1/option1", "round1/option2", "round1/option3"], correctAnswer: 0),
-    GameInfo(round: 2, difficulty: "★★☆", score: 20, paperImage: "round2/foldpaper", mainImage: "round2/main", optionImages: ["round2/option1", "round2/option2", "round2/option3"], correctAnswer: 1),
-    GameInfo(round: 3, difficulty: "★★★", score: 20, paperImage: "round3/foldpaper", mainImage: "round3/main", optionImages: ["round3/option1", "round3/option2", "round3/option3"], correctAnswer: 2),
-    GameInfo(round: 4, difficulty: "★★★", score: 20, paperImage: "round4/foldpaper", mainImage: "round4/main", optionImages: ["round4/option1", "round4/option2", "round4/option3"], correctAnswer: 1)
+    GameInfo(round: 1, difficulty: "★☆☆", score: 20, paperImage: "round0/foldpaper", mainImage: "round0/main", optionImages: ["round0/option1", "round0/option2", "round0/option3"], correctAnswer: 0),
+    GameInfo(round: 2, difficulty: "★★☆", score: 40, paperImage: "round1/foldpaper", mainImage: "round1/main", optionImages: ["round1/option1", "round1/option2", "round1/option3"], correctAnswer: 0),
+    GameInfo(round: 3, difficulty: "★★☆", score: 40, paperImage: "round2/foldpaper", mainImage: "round2/main", optionImages: ["round2/option1", "round2/option2", "round2/option3"], correctAnswer: 1),
+    GameInfo(round: 4, difficulty: "★★★", score: 50, paperImage: "round3/foldpaper", mainImage: "round3/main", optionImages: ["round3/option1", "round3/option2", "round3/option3"], correctAnswer: 2),
+    GameInfo(round: 5, difficulty: "★★★", score: 50, paperImage: "round4/foldpaper", mainImage: "round4/main", optionImages: ["round4/option1", "round4/option2", "round4/option3"], correctAnswer: 1)
 ]
 
 
